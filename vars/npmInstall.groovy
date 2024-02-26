5d3e07dbb5000b134cad5fe92e38dad60fe6b532
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-02-26 13:06:48 +0530

    Update npmInstall.groovy

--- vars/npmInstall.groovy
+++ vars/npmInstall.groovy
@@ -7,5 +7,25 @@ def call() {
 //     def password = 'jagriti@123'
 // sh "echo ${password} | sudo -S apt-get update"
 
-    sh 'sudo apt-get install npm'
+   // sh 'sudo apt-get install npm'
+    // my-shared-library/vars/npmInstall.groovy
+
+    // Replace 'npm install' with any necessary commands to install Node.js and npm on the GCP VM
+    
+    
+    // Optionally, you can check the installed versions of Node.js and npm
+    
+  
+    def startTime = new Date()
+    echo "Starting npm install at ${startTime}"
+    sh 'sudo apt-get update && sudo apt-get install -y nodejs npm'
+   // sh 'npm install'
+    sh 'node -v && npm -v'
+    def endTime = new Date()
+    echo "Finished npm install at ${endTime}"
+    
+    def duration = endTime.time - startTime.time
+    echo "npm install took ${duration} milliseconds"
+}
+
 }
