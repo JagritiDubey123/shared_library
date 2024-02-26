a0df5388316dc14a1c466327e7c685a33e24d468
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-02-26 12:53:10 +0530

    Update npmInstall.groovy

--- vars/npmInstall.groovy
+++ vars/npmInstall.groovy
@@ -4,8 +4,8 @@ def call() {
     // List files in current directory
     sh 'ls -la' 
     // Install npm package
-    def password = 'jagriti@123'
-sh "echo ${password} | sudo -S apt-get update"
+//     def password = 'jagriti@123'
+// sh "echo ${password} | sudo -S apt-get update"
 
     sh 'sudo apt-get install npm'
 }
